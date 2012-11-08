9199092a39ae94ce8a4d2882e715da166b2f04f1
JaceRider <jacerider@gmail.com>
2012-11-08 17:02:54 -0600

    Update plugins/entity/Valet_EntityHandler_User.class.php
    Correct indenting.

--- plugins/entity/Valet_EntityHandler_User.class.php
+++ plugins/entity/Valet_EntityHandler_User.class.php
@@ -2,21 +2,18 @@
 
 class Valet_EntityHandler_User extends Valet_EntityHandler_Abstract {
 
-	protected function db_keys(){
-		return array('uid', 'name');
-	}
-
-  protected function db_result($result){
-  	$uri = $this->entity['uri callback'];
-    $path = $uri($result);
-  	$link = array(
-      'label' => $result->name,
-      'value' => $path['path'],
-  	);
-    return array($path['path'] => $link);
+  protected function db_keys(){
+    return array('uid', 'name');
   }
 
-  protected function db_filter(&$query){
+  protected function db_result($result){
+    $uri = $this->entity['uri callback'];
+    $path = $uri($result);
+    $link = array(
+      'label' => $result->name,
+      'value' => $path['path'],
+    );
+    return array($path['path'] => $link);
   }
 
 }
