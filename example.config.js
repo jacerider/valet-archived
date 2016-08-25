048ffeedc3e07393dfecfc7523f9c63fdcd30baa
Cyle Carlson <jacerider@gmail.com>
2016-08-25 16:51:17 -0500

    Improve gulp workflow.

--- example.config.js
+++ example.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   compileScss: true,
   compileJs: true,
+  compressImages: true,
+  purgeRenderCache: true,
+
   browserSync: {
     enable: true,
     hostname: "localhost",
     port: 8080,
     openAutomatically: true
+  },
+
+  drush: {
+    css_js: 'drush cc css-js',
+    render: 'drush cc render',
+    cr: 'drush cr'
   }
 };
--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,26 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var notify = require('gulp-notify');
+var rename = require('gulp-rename');
+var shell = require('gulp-shell');
+var plumber = require('gulp-plumber');
 var browserSync = require('browser-sync').create();
 var fs = require('fs');
 var config = {
   compileScss: true,
   compileJs: true,
+  compressImages: true,
+  purgeRenderCache: false,
   browserSync: {
     enable: false,
     hostname: null,
     port: 8080,
     openAutomatically: false
+  },
+  drush: {
+    css_js: 'drush cc css-js',
+    render: 'drush cc render',
+    cr: 'drush cr'
   }
 };
 
@@ -28,6 +38,13 @@ var clean = require('gulp-clean-css');
 
 // JavaScript
 var uglify = require('gulp-uglify');
+
+// Images
+var svgmin = require('gulp-svgmin');
+var imagemin = require('gulp-imagemin');
+
+// Other
+var size = require('gulp-size');
 
 // If config.js exists, load that config for overriding certain values below.
 function loadConfig() {
@@ -43,15 +60,24 @@ loadConfig();
 // Compile SCSS
 ////////////////////////////////////////////////////////////////////////////////
 
+gulp.task('sassLint', function (){
+  gulp.src(['./dev/scss/*.scss', './dev/scss/**/*.scss'])
+    .pipe(plumber())
+    .pipe(lint());
+});
+
 gulp.task('sass', function (){
+  var s = size({showTotal: false});
+
   gulp.src(['./dev/scss/*.scss'])
-    .pipe(lint())
+    .pipe(plumber())
+    .pipe(s)
     .pipe(sourcemaps.init())
     .pipe(sass({
       noCache: true,
       outputStyle: 'compressed',
       lineNumbers: false,
-      includePaths: ['./dev/scss']
+      includePaths: ['./dev/scss', './vendor/foundation-sites/scss']
     })).on('error', function(error) {
       gutil.log(error);
       this.emit('end');
@@ -66,7 +92,7 @@ gulp.task('sass', function (){
     .pipe(config.browserSync.enable ? browserSync.stream({match: '**/*.css'}) : gutil.noop())
     .pipe(notify({
       title: "SASS Compiled",
-      message: "All SASS files have been recompiled to CSS.",
+      message: () => `All SASS files have been recompiled to CSS. Total size ${s.prettySize}`,
       onLast: true
     }));
 });
@@ -77,16 +103,38 @@ gulp.task('sass', function (){
 
 gulp.task('js', function (){
   gulp.src(['./dev/js/*.js', './dev/js/**/*.js'])
+    .pipe(plumber())
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('./js'))
-    .pipe(config.browserSync.enable ? browserSync.stream() : gutil.noop())
+    .pipe(browserSync.stream())
     .pipe(notify({
       title: "JS Minified",
       message: "All JS files have been minified.",
       onLast: true
     }));
+});
+
+gulp.task('js-watch', ['js'], browserSync.reload);
+
+////////////////////////////////////////////////////////////////////////////////
+// Compress Images
+////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('svgmin', function() {
+  gulp.src(['./dev/img/*.svg', './dev/img/**/*.svg'])
+    .pipe(plumber())
+    .pipe(svgmin())
+    .pipe(gulp.dest('./images'));
+});
+
+gulp.task('imgmin', function () {
+  gulp.src(['./dev/img/*', './dev/img/**/*'])
+    .pipe(plumber())
+    .pipe(imagemin())
+    .pipe(browserSync.stream())
+    .pipe(gulp.dest('./images'));
 });
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -102,10 +150,56 @@ gulp.task('browser-sync', function() {
 });
 
 ////////////////////////////////////////////////////////////////////////////////
-// Watch Task
+// Drush
 ////////////////////////////////////////////////////////////////////////////////
 
-gulp.task('watch', function(){
+gulp.task('drush:css_js', function () {
+  gulp.src('', {read: false})
+    .pipe(plumber())
+    .pipe(shell([
+      config.drush.css_js
+    ]))
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      title: "Cache cleared",
+      message: "Drupal CSS/JS cache cleared.",
+      onLast: true
+    }));
+});
+
+gulp.task('drush:render', function () {
+  gulp.src('', {read: false})
+    .pipe(plumber())
+    .pipe(shell([
+      config.drush.render
+    ]))
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      title: "Cache cleared",
+      message: "Drupal render cache cleared.",
+      onLast: true
+    }));
+});
+
+gulp.task('drush:cr', function () {
+  gulp.src('', {read: false})
+    .pipe(plumber())
+    .pipe(shell([
+      config.drush.cr
+    ]))
+    .pipe(browserSync.stream())
+    .pipe(notify({
+      title: "Cache rebuilt",
+      message: "Drupal cache rebuilt.",
+      onLast: true
+    }));
+});
+
+////////////////////////////////////////////////////////////////////////////////
+// Default Task
+////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('default', function(){
 
   if (config !== null) {
     if (config.browserSync.enable) {
@@ -113,15 +207,19 @@ gulp.task('watch', function(){
     }
 
     if (config.compileScss) {
-      // gulp.start('sass');
-      gulp.watch(['./dev/scss/*.scss', './dev/scss/**/*.scss'], ['sass']);
+      gulp.watch(['./dev/scss/*.scss', './dev/scss/**/*.scss'], ['sassLint','sass']);
     }
 
     if (config.compileJs) {
-      // gulp.start('js');
       gulp.watch(['./dev/js/*.js', './dev/js/**/*.js'], ['js']);
+    }
+
+    if (config.compressImages) {
+      gulp.watch(['./dev/img/*', './dev/img/**/*'], ['svgmin','imgmin']);
+    }
+
+    if (config.purgeRenderCache) {
+      gulp.watch(['./templates/**/*.html.twig'], ['drush:render']);
     }
   }
 });
-
-gulp.task('default', ['watch']);
